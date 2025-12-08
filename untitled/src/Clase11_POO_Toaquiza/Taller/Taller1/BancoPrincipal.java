41e55971e3e17f8c91b89693e79d4606cbcafb7d
gabrielt007 <gabrieltoaquiza.saenz@gmail.com>
2025-12-08 12:54:59 -0500

    Clase11

---
+++ untitled/src/Clase11_POO_Toaquiza/Taller/Taller1/BancoPrincipal.java
@@ -0,0 +1,10 @@
+package Clase11_POO_Toaquiza.Taller.Taller1;
+
+import javax.swing.*;
+
+public class BancoPrincipal {
+    private JButton depositoButton;
+    private JButton salirButton;
+    private JButton retiroButton;
+    private JButton transferenciaButton;
+}
